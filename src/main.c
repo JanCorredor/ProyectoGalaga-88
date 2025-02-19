f3e636e042d3fb9377654dcb68fa2b98a3add6d5
Arnauwu <arnaugonzalezolive@gmail.com>
2025-02-19 13:04:51 +0100

    logo juego y movimiento basico del personaje

--- src/main.c
+++ src/main.c
@@ -11,6 +11,34 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+//-------------------------------------------------------------------------------------
+// Defines
+//-------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------
+// Types and Structures Definitions
+//-------------------------------------------------------------------------------------
+typedef enum GameScreen 
+{ 
+	LOGO = 0, TITLE, GAMEPLAY, ENDING 
+} GameScreen;
+
+typedef struct Player {
+    Vector2 position;
+    int radius;
+    Color color;
+} Player;
+//-------------------------------------------------------------------------------------
+// Variables Globales
+//-------------------------------------------------------------------------------------
+static Player player = { 0 };
+//-------------------------------------------------------------------------------------
+// Declaracion de funciones
+//-------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------
+// Main
+//-------------------------------------------------------------------------------------
 int main()
 {
 	// Tell the window to use vsync and work on high DPI displays
@@ -24,23 +52,104 @@ int main()
 
 	// Load a texture from the resources directory
 	Texture GalagaTitleLogo = LoadTexture("GalagaTitleLogo.png");
+    GameScreen currentScreen = LOGO;
+    int framesCounter = 0;
+
+    player.radius = 25;
+    player.position = (Vector2){ 80, 100 };
 
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
-		// drawing
+
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            // TODO: Update LOGO screen variables here!
+
+            framesCounter++;    // Count frames
+
+            // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+            if (framesCounter > 120)
+            {
+                currentScreen = GAMEPLAY;
+            }
+        } break;
+        case TITLE:
+        {
+            // TODO: Update TITLE screen variables here!
+
+            // Press enter to change to GAMEPLAY screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                currentScreen = GAMEPLAY;
+            }
+        } break;
+        case GAMEPLAY:
+        {
+            if (IsKeyDown(KEY_UP)) player.position.y -= 12;
+
+            if (IsKeyDown(KEY_DOWN)) player.position.y += 12;
+
+            if (IsKeyDown(KEY_LEFT)) player.position.x -= 12;
+
+            if (IsKeyDown(KEY_RIGHT)) player.position.x += 12;
+
+        } break;
+        case ENDING:
+        {
+            // TODO: Update ENDING screen variables here!
+
+            // Press enter to return to TITLE screen
+            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            {
+                //currentScreen = TITLE;
+            }
+        } break;
+        default: break;
+        }
+
+        //DRAWING
+
+
+
+
+
 		BeginDrawing();
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
 
-		// draw some text using the default font
-		DrawText("Jan Corredor, Alexandre Garcia, Arnau Gonzalez", 75, 750, 45, WHITE);
 
-		// draw our texture to the screen
-		DrawTexture(GalagaTitleLogo, 0, 0, WHITE);
+        switch (currentScreen)
+        {
+        case LOGO:
+        {
+            ClearBackground(BLACK);
+            DrawText("Jan Corredor, Alexandre Garcia, Arnau Gonzalez", 75, 750, 45, WHITE);
+            DrawTexture(GalagaTitleLogo, 0, 0, WHITE);
+        } break;
+        case TITLE:
+        {
 
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+
+        } break;
+        case GAMEPLAY:
+        {
+            ClearBackground(BLACK);
+
+            DrawCircle(player.position.x, player.position.y, player.radius, BLUE);
+
+
+        } break;
+        case ENDING:
+        {
+
+
+        } break;
+        default: break;
+        }
+
+
 		EndDrawing();
 	}
 
@@ -52,3 +161,6 @@ int main()
 	CloseWindow();
 	return 0;
 }
+//-------------------------------------------------------------------------------------
+// Funciones 
+//-------------------------------------------------------------------------------------
