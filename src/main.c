202631058d0c04915b3038acc5424f23d207a52b
JanCorredor <jan.corredor@estudiantat.upc.edu>
2025-02-21 09:57:47 +0100

    Clase
    Actualizar Movement
    Sistema Particulas
    Title Screen

--- src/main.c
+++ src/main.c
@@ -23,18 +23,42 @@ typedef enum GameScreen
 	LOGO = 0, TITLE, GAMEPLAY, ENDING 
 } GameScreen;
 
+//Title + Gameplay
+typedef struct {
+    Vector2 particle_position;
+    Color particle_color;
+    int opacity;
+    int particle_size;
+} Particle;
+
+//Gameplay
 typedef struct Player {
     Vector2 position;
     int radius;
     Color color;
 } Player;
+
+typedef struct Bullet {
+    Vector2 bullet_position;
+    int bullet_radius;
+    Color bullet_color;
+} Bullet;
+
+
+
 //-------------------------------------------------------------------------------------
-// Variables Globales
+// Global Variables 
 //-------------------------------------------------------------------------------------
 static Player player = { 0 };
+
 //-------------------------------------------------------------------------------------
 // Declaracion de funciones
 //-------------------------------------------------------------------------------------
+
+#define MAXPARTICLES 50
+Particle particles[MAXPARTICLES];
+void createParticles();
+void drawParticles();
 
 //-------------------------------------------------------------------------------------
 // Main
@@ -50,15 +74,30 @@ int main()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
+	// Load textures from the resources directory
 	Texture GalagaTitleLogo = LoadTexture("GalagaTitleLogo.png");
+    Texture Galaga88Logo = LoadTexture("GALAGA88_LOGO.png");
     GameScreen currentScreen = LOGO;
-    int framesCounter = 0;
+    
+    //Frame
+    unsigned int framesCounter = 0;
+    SetTargetFPS(60); // Set Game to run 60 Frames per second
 
+    //Particles
+    createParticles();
+
+    //-------------------------------------------------------------------------------------
+    // Local Variables
+    //-------------------------------------------------------------------------------------
+    //Player
     player.radius = 25;
-    player.position = (Vector2){ 80, 100 };
+    player.position = (Vector2){ GetScreenWidth()/2, GetScreenHeight()*9/10 };
 
-	// game loop
+
+
+    //-------------------------------------------------------------------------------------
+    // Game Loop
+    //-------------------------------------------------------------------------------------
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 
@@ -73,7 +112,7 @@ int main()
             // Wait for 2 seconds (120 frames) before jumping to TITLE screen
             if (framesCounter > 120)
             {
-                currentScreen = GAMEPLAY;
+                currentScreen = TITLE;
             }
         } break;
         case TITLE:
@@ -81,20 +120,35 @@ int main()
             // TODO: Update TITLE screen variables here!
 
             // Press enter to change to GAMEPLAY screen
-            if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+            if (IsKeyPressed(KEY_ENTER)) //  || IsGestureDetected(GESTURE_TAP)
             {
                 currentScreen = GAMEPLAY;
             }
         } break;
         case GAMEPLAY:
         {
-            if (IsKeyDown(KEY_UP)) player.position.y -= 12;
-
-            if (IsKeyDown(KEY_DOWN)) player.position.y += 12;
-
+            //Player Movement
             if (IsKeyDown(KEY_LEFT)) player.position.x -= 12;
-
             if (IsKeyDown(KEY_RIGHT)) player.position.x += 12;
+
+            //Player Collisions
+            if ((player.position.x + player.radius) >= GetScreenWidth()) //Right Side
+            {
+                player.position.x = GetScreenWidth() - player.radius;
+            }
+            else if ((player.position.x - player.radius) <= 0) //Left Side
+            {
+                player.position.x = 0 + player.radius;
+            }
+
+            //Shoot
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                //Crear Instancia de bala
+            }
+
+
+
 
         } break;
         case ENDING:
@@ -130,6 +184,23 @@ int main()
         } break;
         case TITLE:
         {
+            ClearBackground(BLACK);
+
+            //Particulas
+            drawParticles();
+
+            //Logo
+            DrawTexture(Galaga88Logo, GetScreenWidth() / 3, GetScreenHeight()/ 10, WHITE);
+
+            //Scores
+            DrawText("SCORE", GetScreenWidth() / 20, GetScreenHeight() / 50, 45, WHITE);
+            DrawText("H I SCORE", GetScreenWidth() *7/ 10, GetScreenHeight() / 50, 45, WHITE);
+
+            //Other
+            DrawText("PUSH ENTER", GetScreenWidth()/3, GetScreenHeight() /2, 45, GREEN);
+
+
+
 
 
         } break;
@@ -164,3 +235,28 @@ int main()
 //-------------------------------------------------------------------------------------
 // Funciones 
 //-------------------------------------------------------------------------------------
+
+void createParticles()
+{
+    Color particle_colors[6] = { GREEN, SKYBLUE, BLUE, RED, WHITE, LIGHTGRAY }; //Particles Possible Colors
+    for (int i = 0; i < MAXPARTICLES; i++) //Create
+    {
+        particles[i].particle_position = (Vector2){ GetRandomValue(GetScreenWidth()/100, GetScreenWidth()*99/100), 0};
+        particles[i].particle_color = particle_colors[GetRandomValue(0, 5)];
+        particles[i].particle_size = 5;
+        particles[i].opacity = 100;
+    }
+}
+
+void drawParticles()
+{
+    for (int i = 0; i < MAXPARTICLES; i++) //Update
+    {
+        particles[i].particle_position.y += 1;
+    }
+
+    for (int i = 0; i < MAXPARTICLES; i++) //Draw
+    {
+        DrawRectangle(particles[i].particle_position.x, particles[i].particle_position.y, particles[i].particle_size, particles[i].particle_size, particles[i].particle_color);
+    }
+}
